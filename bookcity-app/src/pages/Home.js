791ce71b8e846ddbb48115c4cb2f359ab0d1dc1a
mahboob <mahboob6103@gmail.com>
2022-05-05 00:15:59 +0430

    send edit

--- bookcity-app/src/pages/Home.js
+++ bookcity-app/src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Link,Outlet} from "react-router-dom"
 export default function Home() {
   return (
-    <div style={{width:'100vw', height:'70vh'}}>Home
+    <div style={{ height:'70vh'}}>Home
    
     <Outlet /> 
     </div>
