a4cbbb6326cc3c3a5140965eb0e8691eb61d2a7c
mahboob <mahboob6103@gmail.com>
2022-05-10 14:33:28 +0430

    feat:make header for user

--- src/layout/Header/HeaderUser.js
+++ src/layout/Header/HeaderUser.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
 
+import style from "../../assets/styles/style.css";
+import Logo from "../../componentes/logo/Logo";
+import SearchBox from "../../componentes/SearchBox/SearchBox";
+
+import {PATHS} from '../../config/routes.config'
+import {Link} from 'react-router-dom'
+import { Box, Grid, Button } from "@mui/material";
+import basket from "../../assets/images/basket.svg";
+import user from '../../assets/images/user.svg'
 export default function HeaderUser() {
   return (
-    <h2 style={{width:'98vw',height:'10vh',background:'orange',textAlign:'center'}}>
-      
-    HeaderUser
-      
-    </h2>
-  )
+    <Box sx={{ flexGrow: 1 }} className="header"  alignItems="center">
+      <Grid
+        container
+        rowSpacing={0}
+        
+        justifyContent="space-around"
+        alignItems="center"
+      >
+        <Grid item xs={2} >
+          <Logo />
+        </Grid>
+        <Grid item xs={7} >
+          <SearchBox />
+        </Grid>
+        <Grid item xs={1} sx={{ p: 2 }}>
+          <img src={basket} />
+          <h5>سبد خرید</h5>
+        </Grid>
+        <Grid item xs={1} sx={{ p: 2 }} >
+          <img src={user} />
+         <h4><Link style={{textDecoration:'none'}} to={PATHS.LOGIN}>مدیریت</Link>  </h4> 
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
