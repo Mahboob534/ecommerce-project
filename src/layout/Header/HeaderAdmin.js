c10370d8c889baf27188d636c0bd0b8c474c898c
mahboob <mahboob6103@gmail.com>
2022-05-12 05:03:10 +0430

    feat:design header for admin

--- src/layout/Header/HeaderAdmin.js
+++ src/layout/Header/HeaderAdmin.js
@@ -1,12 +1,46 @@
-import React from 'react'
+import React,{useState,useEffect} from "react";
+
+import style from "../../assets/styles/style.css";
+import Logo from "../../componentes/logo/Logo";
+
+import { PATHS } from "../../config/routes.config";
+import { Link } from "react-router-dom";
+import { Box, Grid, Button, ButtonGroup } from "@mui/material";
+import basket from "../../assets/images/basket.svg";
+import user from "../../assets/images/user.svg";
+import { Dashboard } from "@mui/icons-material";
+import {useSelector , useDispatch} from 'react-redux'
+import {setProduct} from '../../redux/action/ProductSlice'
 
 export default function HeaderAdmin() {
+  
   return (
-    <div style={{width:'98vw',height:'10vh',background:'yellow'}}>
-      
-     <h2 style={{textAlign:'center'}} >HeaderAdmin</h2> 
-      
-      
-      </div>
-  )
+    <div>
+      <Box sx={{ flexGrow: 1 }} className="header" alignItems="center">
+        <Grid
+          container
+          rowSpacing={1}
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Grid item xs={4}>
+            <Logo />
+          </Grid>
+          <Grid item xs={8} justifyContent="center">
+            <ButtonGroup  size="large" color='secondary'
+              
+              aria-label="outlined primary button group"
+            
+            >
+              
+              <Button  value =" product" variant="contained" sx={{p:5}} onClick={e => <Dashboard rows={e.target.value}/>}>کالا</Button>
+              <Button  value ="stack" variant="contained" sx={{p:5}} onClick={e => <Dashboard stack = {e.target.value}/> }>موجودیت </Button>
+              <Button  value ="order" variant="contained" sx={{p:5}} onClick={e => <Dashboard order={e.target.value}/>}>سفارش</Button>
+            </ButtonGroup>
+          </Grid>
+          
+        </Grid>
+      </Box>
+    </div>
+  );
 }
