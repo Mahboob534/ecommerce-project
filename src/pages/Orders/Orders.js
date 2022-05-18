65e2d6139ff25f0353b9a42b505fe9df2cc0d170
mahboob <mahboob6103@gmail.com>
2022-05-18 12:29:08 +0430

    feat: make order table and filtering

--- src/pages/Orders/Orders.js
+++ src/pages/Orders/Orders.js
@@ -1,7 +1,72 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import instanceAxios from "../../Utils/axios";
+import {
+  Box,
+  Grid,
+  FormControl,
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  FormLabel,
+} from "@mui/material";
+import TableOrders from "./components/TableOrders";
+import axios from "axios";
 
-export default function Orders() {
+function Orders() {
+  const [row, setRow] = useState([]);
+
+  const [selectedValue, setSelectedValue] = React.useState("a");
+
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value);
+  };
+
+  // const getorders = async () => {
+  //   const res = await fetch(
+  //     `http://localhost:3002/orders?orderStatus=${selectedValue}`
+  //   );
+  //   const data = await res.json();
+  //   setRow(data);
+  // };
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3002/orders?orderStatus=${selectedValue}`)
+      .then((res) => setRow(res.data));
+  }, [selectedValue]);
+
+  console.log(row);
+
   return (
-    <div>order</div>
-  )
+    <Grid container  >
+      <Grid item xs={12} md={8}>
+        <FormControl>
+          <RadioGroup
+            row
+            aria-labelledby="demo-row-radio-buttons-group-label"
+            name="row-radio-buttons-group"
+            sx={{ mr:30, mt:5 }}
+          >
+            <FormControlLabel
+              onChange={handleChange}
+              value="1"
+              control={<Radio />}
+              label="سفارشات تحویل داده شده"
+              sx={{mr:10}}
+            />
+            <FormControlLabel
+              onChange={handleChange}
+              value="3"
+              control={<Radio />}
+              label="سفارشات در حال تحویل "
+              sx={{mr:10}}
+            />
+          </RadioGroup>
+        </FormControl>
+      </Grid>
+      <Grid item xs={12} md={8}>
+        <TableOrders row={row} />
+      </Grid>
+    </Grid>
+  );
 }
+export { Orders };
