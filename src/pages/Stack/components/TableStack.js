2ef6444109e3a1ded21c447e27230b31c4ab0129
mahboob <mahboob6103@gmail.com>
2022-05-19 07:04:07 +0430

    feat:adding code to convert cell table to input

--- src/pages/Stack/components/TableStack.js
+++ src/pages/Stack/components/TableStack.js
@@ -1,21 +1,20 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import { useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableFooter from '@mui/material/TableFooter';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import IconButton from '@mui/material/IconButton';
-import FirstPageIcon from '@mui/icons-material/FirstPage';
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
-import LastPageIcon from '@mui/icons-material/LastPage';
-
+import * as React from "react";
+import PropTypes from "prop-types";
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableFooter from "@mui/material/TableFooter";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import IconButton from "@mui/material/IconButton";
+import FirstPageIcon from "@mui/icons-material/FirstPage";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import LastPageIcon from "@mui/icons-material/LastPage";
 
 function TablePaginationActions(props) {
   const theme = useTheme();
@@ -38,36 +37,44 @@ function TablePaginationActions(props) {
   };
 
   return (
-    <Box sx={{ flexShrink: 0, display:"flex", justifyContent:"space-between"}}>
+    <Box
+      sx={{ flexShrink: 0, display: "flex", justifyContent: "space-between" }}
+    >
       <IconButton
-      
         onClick={handleFirstPageButtonClick}
         disabled={page === 0}
         aria-label="first page"
       >
-        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+        {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
       </IconButton>
       <IconButton
-     
         onClick={handleBackButtonClick}
         disabled={page === 0}
         aria-label="previous page"
       >
-        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowRight />
+        ) : (
+          <KeyboardArrowLeft />
+        )}
       </IconButton>
       <IconButton
         onClick={handleNextButtonClick}
         disabled={page >= Math.ceil(count / rowsPerPage) - 1}
         aria-label="next page"
       >
-        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowLeft />
+        ) : (
+          <KeyboardArrowRight />
+        )}
       </IconButton>
       <IconButton
         onClick={handleLastPageButtonClick}
         disabled={page >= Math.ceil(count / rowsPerPage) - 1}
         aria-label="last page"
       >
-        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+        {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
       </IconButton>
     </Box>
   );
@@ -85,11 +92,13 @@ function TablePaginationActions(props) {
 // }
 
 export default function CustomPaginationActionsTable(props) {
-  let rows= props.row
-
+  let rows = props.row;
+ // let flag= props.flag
+  //let setFlag=props.setFlag
+  const[flag,setFlag]=useState(true)
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
-
+  
   // Avoid a layout jump when reaching the last page with empty rows.
   const emptyRows =
     page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
@@ -104,31 +113,44 @@ export default function CustomPaginationActionsTable(props) {
   };
 
   return (
-    <TableContainer component={Paper}  sx={{direction:'rtl',mr:30,width:"60vw",height:"60vh"}} >
-      <Table  aria-label="custom pagination table">
-        <TableBody sx={{direction:'rtl' }}>
-        <TableRow key={1}>
-              <TableCell style={{ width: 40 }} align="right">
-          کالا    
-                    </TableCell>
-              <TableCell style={{ width: 40 }} component="th" scope="row" align="right">
-               قیمت
-              </TableCell>
-              <TableCell style={{ width: 40 }} align="right">
+    <TableContainer
+      component={Paper}
+      sx={{ direction: "rtl", mr: 30, width: "60vw", height: "60vh" }}
+    >
+      <Table aria-label="custom pagination table">
+        <TableBody sx={{ direction: "rtl" }}>
+          <TableRow key={1}>
+            <TableCell style={{ width: 40 }} align="right">
+              کالا
+            </TableCell>
+            <TableCell
+              style={{ width: 40 }}
+              component="th"
+              scope="row"
+              align="right"
+            >
+              قیمت
+            </TableCell>
+            <TableCell style={{ width: 40 }} align="right">
               موجودی
-              </TableCell>
-              
-            </TableRow>
-          
+            </TableCell>
+          </TableRow>
+
           {(rowsPerPage > 0
             ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
             : rows
           ).map((row) => (
             <TableRow key={row.name}>
               <TableCell style={{ width: 40 }} align="right">
-              {row.name}
+                {row.name}
               </TableCell>
-              <TableCell style={{ width: 30 }} component="th" scope="row" align="right">
+              <TableCell
+                style={{ width: 30 }}
+                component={flag ? "th" : "Input"}
+                scope="row"
+                align="right"
+                onClick={() =>setFlag(false)}
+              >
                 {row.price}
               </TableCell>
               <TableCell style={{ width: 30 }} align="right">
@@ -143,21 +165,19 @@ export default function CustomPaginationActionsTable(props) {
             </TableRow>
           )}
         </TableBody>
-        <TableFooter >
+        <TableFooter>
           <TableRow>
             <TablePagination
-              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
-              
+              rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
               colSpan={3}
               count={rows.length}
               rowsPerPage={rowsPerPage}
               page={page}
               SelectProps={{
                 inputProps: {
-                  'aria-label': 'rows per page',
+                  "aria-label": "rows per page",
                 },
                 native: true,
-                
               }}
               onPageChange={handleChangePage}
               onRowsPerPageChange={handleChangeRowsPerPage}
