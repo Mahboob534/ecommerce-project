b1e4d8d16e61a26009733f971295b5d9dcee1c8d
mahboob <mahboob6103@gmail.com>
2022-05-19 06:42:05 +0430

    feat:make modal for adding product

--- src/componentes/From/Form.js
+++ src/componentes/From/Form.js
@@ -1,7 +1,133 @@
-import React from 'react'
+import React, { useState } from "react";
+import { ACCESS_TOKEN } from "../../config/variable.config";
+import {
+  Button,
+  TextField,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  Select,
+  Grid,
+} from "@mui/material";
+import axios from "axios";
+
+const skills = ["داستان", "آشپزی", "آموزش", "ادبیات", "هنری"];
 
 export default function Form() {
+  const [value, setValue] = useState();
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const handleChange = (e) => {
+    if (e.target.name !== "thumbnail") {
+      setValue({ ...value, [e.target.name]: e.target.value });
+    } else {
+      let files = e.target.files[0];
+      console.log("thumbnail", e.target.files);
+      setValue({ ...value, [e.target.name]: files });
+    }
+  };
+console.log(value);
+ async function submit() {
+    const formData = new FormData();
+    Object.entries(value).map((entry) => {
+      console.log(entry);
+      formData.append(entry[0], entry[1]);
+      })
+
+  try {
+    // make axios post request
+    const response = await axios({
+      method: "post",
+      url: "http://localhost:3002/upload",
+      data: formData['thumbnail', File],
+      headers: {  Authorization:`${ACCESS_TOKEN}` },
+    });
+  } catch(error) {
+    console.log(error)
+  }
+}
+
+    
+    
+    // Object.entries(formData).map((item)=>{
+    //   const res = axios
+    //   .post("http://localhost:3002/upload", item ,{
+    //     headers:{
+    //      Authorization:`${ACCESS_TOKEN}`
+          
+    //     }
+    //   })
+    //   .then((res) => {
+    //     console.log(res);
+    //   });
+    // })
+      
+
+    
+
+        
+      
+      
+      // fetch('http://localhost:3002/upload', {
+      //   method: 'POST',
+      //   body: formData
+      // }).then(res=> res.json()).then(res=>{
+      //     console.log(res)
+      // })
+
+  
   return (
-    <div>Form</div>
-  )
+    <Grid container sx={{width:600}}>
+      <Grid item xs={12} md={8}>
+        <form>
+          <TextField
+            label="تصویر کالا"
+            name="thumbnail"
+            type="file"
+            
+            onChange={(e) => handleChange(e)}
+            margin="normal"
+            fullWidth
+          />
+
+          <br />
+          <TextField
+            label=" نام کالا"
+            name="name"
+            type="text"
+            onChange={(e) => handleChange(e)}
+            margin="normal"
+            fullWidth
+          />
+          <br />
+          <FormControl fullWidth>
+            <InputLabel htmlFor="categoryLbl">دسته بندی کالا</InputLabel>
+            <Select name="category" fullWidth
+            
+             onChange={(e) => handleChange(e)}>
+              {skills.map((category) => (
+                <MenuItem key={category} value={category}>
+                  {category}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <br />
+          <TextField
+            label="توضیحات"
+            name="description"
+            multiline
+            rows="4"
+            onChange={(e) => handleChange(e)}
+            margin="normal"
+            fullWidth
+          />
+          <br />
+          <Button color="primary" variant="contained" onClick={submit}>
+            ذخیره
+          </Button>
+        </form>
+      </Grid>
+    </Grid>
+  );
 }
---
+++ src/componentes/Modal/ModalProduct.js
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import Form from "../../componentes/From/Form"
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function ModalProduct() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Button sx={{backgroundColor:"#86efac", width:'100px', m:3 }} variant="outlined" onClick={handleOpen}> افزودن کالا</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h4" component="h2">
+          ویرایش و افزودن کالا
+          
+          <Form/>
+          </Typography>
+         
+        </Box>
+       
+      </Modal>
+    </div>
+  );
+}
