44a510c68c2ada76a35321e95b7215f924f3ea0d
mahboob <mahboob6103@gmail.com>
2022-05-19 08:31:52 +0430

    feat:make page subcategories

--- src/pages/Categories/Categories.js
+++ src/pages/Categories/Categories.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
+import Category from '../../componentes/Category/Category'
+import {NavLink,Navigate} from 'react-router-dom'
+import LayoutUser from '../../layout/LayoutUser'
+import Grid from '@mui/material/Grid';
+import { useSelector } from "react-redux";
+import axios from 'axios'
+ function Categories() {
+  let selectedId = useSelector((state) => state.category);
 
-export default function Categories() {
+  console.log( selectedId);
   return (
-    <div style={{width:'100vw', height:'70vh'}}>SubGroup</div>
+    <LayoutUser >
+   <Grid container spacing={1} justifyContent="center" sx={{m:5}}>
+    
+       <Category idCategory={selectedId.category} /> 
+                
+    </Grid>
+    </LayoutUser>   
   )
 }
+export {Categories}
+
