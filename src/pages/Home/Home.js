2388c510485ad31020e4ee547840cedd8f3e1548
mahboob <mahboob6103@gmail.com>
2022-05-31 14:12:24 +0430

    fix: home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,13 +2,18 @@ import React,{useState,useEffect} from 'react'
 import Styles from './home.module.css'
 import LayoutUser from '../../layout/LayoutUser'
 import Category from '../../componentes/Category/Category'
-import {NavLink,Navigate} from 'react-router-dom'
+import {Link,Navigate, Outlat} from 'react-router-dom'
 import Grid from '@mui/material/Grid';
-import axios from 'axios'
+import getAllCategory from '../../api/getAll/getAllCategory'
 import { PATHS} from  '../../config/routes.config'
 import { useDispatch } from "react-redux";
 import {setCategory} from '../../redux/action/CategorySlice'
+import Categories from '../Categories/Categories'
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
  function Home() {
+
+
   const [rowCat, setRowCat] = useState([]);
   const dispatch=useDispatch()
   useEffect(() => {
@@ -17,26 +22,36 @@ import {setCategory} from '../../redux/action/CategorySlice'
   }, []);
   async function getData() {
     try {
-      const category = await axios.get("http://localhost:3002/category");
+      const category = await getAllCategory()
         setRowCat(category.data);
     } catch (error) {
      alert("loading");
     }
   }
-
+console.log(rowCat);
   return (
     <LayoutUser >
-     <Grid container  justifyContent="right" >
-    { rowCat ? rowCat.map((item ,id) => (
-              <>
-                <NavLink  to={PATHS.CATEGORIES} className={Styles.titleCat} onClick={()=>dispatch(setCategory(item.id))}>
+      <ToastContainer/>
+     <Grid container  justifyContent="right" sx={{m:2}} >
+      
+       {rowCat ? rowCat.map((item ,id) => (
+              <Grid sx={{border:1,borderColor:"grey.400", p:2,borderRadius:5,m:2 }}>
+                 <Link  to={`/Categories/${item.id}`} className={Styles.titleCat}  id={item.id} onClick={()=>dispatch(setCategory(item.id))}>
                    <h3 key={id}> {item.name}</h3>
-                 </NavLink>
-                 <Category idCategory={item.id} key={id} /></>
+                  
+                 </Link> 
+                 
+                 <Category idCategory={item.id} key={id} />
+                  </Grid>
                  ))
               
-              : <Navigate to='/'/>
+                 : <Navigate to='/'/>
             }
+
+
+      
+    
+            
     </Grid>
     
     </LayoutUser>
